d31014c9547ccaa7747a85e83d881aaf4d2672c8
Akshay Jain <akshay@Akshays-MacBook-Pro.local>
2019-12-11 10:53:46 +0000

    Models

---
+++ Mochi/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Mochi
+//
+//  Created by Akshay Jain on 11/12/2019.
+//  Copyright © 2019 Akshay Jain. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
